781ed4d3adc0fdfc8c168be5bd44bb2afec0de94
Ilya Melamed <ilya77@gmail.com>
2018-12-16 22:33:43 +0200

    Document StreamElementsControllerServer class

--- streamelements/StreamElementsControllerServer.hpp
+++ streamelements/StreamElementsControllerServer.hpp
@@ -5,6 +5,32 @@
 
 class StreamElementsMessageBus;
 
+///////////////////////////////////////////////////////////////////////
+//
+// Listens for JSON messages over a network connection (currently
+// implemented as Named Pipes server).
+//
+// In case the messages are properly structured, dispatches them to
+// all browsers except Browser Sources as an onHostMessageReceived
+// event.
+//
+// For the message to be properly structured, it must contain the
+// following root-level properties:
+//
+//	version		- number. protocol version (1)
+//	source		- object
+//	target		- object ({ scope: "broadcast" })
+//	payload		- object. the message payload.
+//
+// In case of a command message, the "payload" property value is
+// structured as follows:
+//
+//	class		- string. "command"
+//	command		- object. ({ name: "command-name", "data": object })
+//
+// The primary purpose of this class is to support integration with
+// external controllers, such as Elgato Streamdeck.
+//
 class StreamElementsControllerServer
 {
 public:
@@ -12,17 +38,39 @@ public:
 	virtual ~StreamElementsControllerServer();
 
 public:
+	///
+	// Send a message with specified payload to all connected clients
+	//
+	//	source		- message source type
+	//	sourceAddress	- message source address
+	//	payload		- message payload
+	//
 	void NotifyAllClients(
 		std::string source,
 		std::string sourceAddress,
 		CefRefPtr<CefValue> payload);
 
+	///
+	// Send an event message to all connected clients
+	//
+	//	source		- message source type
+	//	sourceAddress	- message source address
+	//	eventName	- event name
+	//	eventData	- event data
+	//
 	void SendEventAllClients(
 		std::string source,
 		std::string sourceAddress,
 		std::string eventName,
 		CefRefPtr<CefValue> eventData);
 
+	///
+	// Send a generic message to all connected clients
+	//
+	//	source		- message source type
+	//	sourceAddress	- message source address
+	//	message		- message data
+	//
 	void SendMessageAllClients(
 		std::string source,
 		std::string sourceAddress,
